82392d1d52b8ddf55ea4ef7a65be18ea7ddbae08
qotkdgus <31401568+qotkdgus@users.noreply.github.com>
2017-08-29 16:48:52 +0900

    보스

---
+++ Assets/Script/Bossmove.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bossmove : MonoBehaviour {
+	enum BOSSSTATE
+	{
+		IDEL ,
+		WALK ,
+		RUN,
+		ATTACK,
+		DEAD
+	}
+
+	BOSSSTATE state =BOSSSTATE.IDEL;
+
+	float stateTime = 0.0f;
+	public float idleStateMaxTime = 2.0f;
+	public float speed = 3.0f;
+	public float rot = 20.0f;
+	public float attackrange = 10f;
+	public float sightrange = 50f;
+
+	public float attackstatemaxtime =3f;
+
+	public int attackpoint = 15;
+	public int attackpoint2 = 30;
+
+	Transform target;
+	CharacterController characterControl;
+
+	public Animator anim;
+	int idle;
+	int attack;
+	int walk;
+	int die;
+	int run;
+	void Awake()
+	{	
+		anim = GetComponent<Animator> ();
+		idle = Animator.StringToHash("axe|idle");
+		attack = Animator.StringToHash("axe|attack");
+		walk = Animator.StringToHash("axe|walk");
+		die = Animator.StringToHash("axe|dead2");
+		run = Animator.StringToHash ("axe|run");
+	}
+
+	void OnEnable ()
+	{
+		InitBoss ();
+	}
+	void Start () {
+		target = GameObject.Find ("2Handed Warrior").transform;
+		characterControl = GetComponent<CharacterController> ();
+	}
+
+	void Update () 
+	{
+		Debug.Log ("Now State : " + state);
+
+		switch (state) {
+		case BOSSSTATE.IDEL:
+			IDEL ();
+			break;
+
+		case BOSSSTATE.WALK:
+			WALK ();
+			break;
+			/*
+		case BOSSSTATE.RUN:
+			RUN ();			
+			break;
+*/
+		case BOSSSTATE.ATTACK:
+			ATTACK ();
+			break;
+
+		case BOSSSTATE.DEAD:
+			DEAD ();			
+			break;
+		}
+
+	}
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (state==BOSSSTATE.DEAD)
+			return;
+		Debug.Log ("Hit");
+		/*
+		if (col.gameObject.tag == "Eye") 
+		{
+			Debug.Log ("EYE");
+			state = BOSSSTATE.DAMAGE1;
+		}
+		else if (col.gameObject.tag == "Boom") 
+		{
+			state = BOSSSTATE.DAMAGE2;
+		}
+		*/
+
+	}
+
+	void IDEL()
+	{
+
+		float distance = (target.position - transform.position).magnitude;
+		if (distance < sightrange) 
+		{
+			stateTime = 0.0f;
+			state = BOSSSTATE.WALK;
+		}
+	}
+
+	void WALK()
+	{
+
+
+		float distance = (target.position - transform.position).magnitude;
+		anim.SetBool ("walk",true);
+		if (distance < attackrange) 
+		{
+			anim.SetBool ("walk",false);
+			stateTime = attackstatemaxtime;
+			state = BOSSSTATE.ATTACK;
+
+		}
+		Vector3 dir = target.position - transform.position;
+		dir.y = 0.0f;
+		dir.Normalize ();
+		characterControl.SimpleMove (dir * speed);
+		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (dir), rot * Time.deltaTime);
+
+	}
+	void RUN()
+	{
+		anim.SetBool ("run",true);
+	}
+	void ATTACK()
+	{
+		stateTime += Time.deltaTime;
+
+		if (stateTime > attackstatemaxtime) {
+
+			anim.SetBool ("attack", true);
+
+			stateTime = 0.0f;
+
+		} else {
+			anim.SetBool ("attack", false);
+		}
+		float distance = (target.position - transform.position).magnitude;
+		if (distance > attackrange) {
+			state = BOSSSTATE.WALK;
+			anim.SetBool ("attack", false);
+		}
+
+	}
+
+	void DEAD()
+	{
+		anim.SetTrigger ("dead2");
+	}
+
+	public void InitBoss()
+	{
+
+		state = BOSSSTATE.IDEL;
+
+	}
+}
--- Assets/Script/cameramove.cs
+++ Assets/Script/cameramove.cs
@@ -1,12 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class SmoothFollow : MonoBehaviour
-{
+public class cameramove : MonoBehaviour {
+
 	#region Consts
 	private const float SMOOTH_TIME = 0.3f;
 	#endregion
-	float x;
-	float cameraspeed =7.0f;
+
 	#region Public Properties
 	public bool LockX;
 	float offSetZ;
@@ -15,23 +16,23 @@ public class SmoothFollow : MonoBehaviour
 	public bool useSmoothing;
 	public int player;
 	Transform target;
-	Transform target2;
+
 	public GameObject hudElements;
 	#endregion
-	
+
 	#region Private Properties
 	private Transform thisTransform;
 	private Vector3 velocity;
 	#endregion
 
 	bool hudActive = true;
-	
+
 	private void Awake()
 	{
 		thisTransform = transform;
 		velocity = new Vector3(0.5f, 0.5f, 0.5f);
 		target = GameObject.Find ("2Handed Warrior").transform;
-		target2 = GameObject.Find ("2Handed Warrior").GetComponentInChildren<Transform> ();
+
 		player = 1;
 	}
 
@@ -57,7 +58,7 @@ public class SmoothFollow : MonoBehaviour
 		if (Input.GetKeyDown (KeyCode.F1)) 
 		{
 			target = GameObject.Find ("2Handed Warrior").transform;
-			target2 = GameObject.Find ("2Handed Warrior").GetComponentInChildren<Transform> ();
+
 			player = 1;
 			Debug.Log ("1");
 		} 
@@ -65,65 +66,25 @@ public class SmoothFollow : MonoBehaviour
 		{
 			Debug.Log ("2");
 			target = GameObject.Find ("Mage Warrior").transform;
-			target2 = GameObject.Find ("Mage Warrior").GetComponentInChildren<Transform> ();
+
 			player = 2;
 		}
 		else if (Input.GetKeyDown (KeyCode.F3)) 
 		{
 			Debug.Log ("3");
 			target = GameObject.Find ("Archer Warrior").transform;
-			target2 = GameObject.Find ("Archer Warrior").GetComponentInChildren<Transform> ();
+
 			player = 3;
 		}
 	}
 
 	// ReSharper disable UnusedMember.Local
 	private void LateUpdate()
-		// ReSharper restore UnusedMember.Local
+	// ReSharper restore UnusedMember.Local
 	{
 		var newPos = Vector3.zero;
-		if (Input.GetMouseButton(1)) 
-		{
-
-			if (useSmoothing)
-			{
-				newPos.x = Mathf.SmoothDamp(thisTransform.position.x, target.position.x, ref velocity.x, SMOOTH_TIME);
-				newPos.y = Mathf.SmoothDamp(thisTransform.position.y, target.position.y +3, ref velocity.y, SMOOTH_TIME);
-				newPos.z = Mathf.SmoothDamp(thisTransform.position.z, target.position.z+2, ref velocity.z, SMOOTH_TIME);
-			}
-			else
-			{
-				newPos.x = target.position.x;
-				newPos.y = target.position.y;
-				newPos.z = target.position.z;
-			}
-
-			#region Locks
-			if (LockX)
-			{
-				newPos.x = thisTransform.position.x;
-			}
-
-			if (LockY)
-			{
-				newPos.y = thisTransform.position.y;
-			}
-
-			if (LockZ)
-			{
-				newPos.z = thisTransform.position.z;
-			}
-			#endregion
-
-			transform.position = Vector3.Slerp (transform.position, newPos, Time.time);
-		}
 
 
-	else
-	{
-			x = Input.GetAxis ("Mouse X");
-			transform.RotateAround (target2.position, -target2.up, x * cameraspeed);
-			/*
 			offSetZ = -6;
 		if (useSmoothing)
 		{
@@ -154,8 +115,9 @@ public class SmoothFollow : MonoBehaviour
 			newPos.z = thisTransform.position.z;
 		}
 		#endregion
-*/
-		//transform.position = Vector3.Slerp(transform.position, newPos, Time.time);
+
+		transform.position = Vector3.Slerp(transform.position, newPos, Time.time);
+
+
 	}
-}
 }
