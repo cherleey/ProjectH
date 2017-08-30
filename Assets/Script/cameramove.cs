f5d2d1543c78115f3c2b54d5d74b83ea74b37c2d
cherleey <31400524+cherleey@users.noreply.github.com>
2017-08-30 11:39:00 +0900

    3인칭 카메라

--- Assets/Script/cameramove.cs
+++ Assets/Script/cameramove.cs
@@ -2,122 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameramove : MonoBehaviour {
+public class CameraMove : MonoBehaviour {
 
-	#region Consts
-	private const float SMOOTH_TIME = 0.3f;
-	#endregion
+	float fDistance = 10.0f;
+	float fX = 0.0f;
+	float fY = 0.0f;
+	float fCameraSpeed = 2.0f;
 
-	#region Public Properties
-	public bool LockX;
-	float offSetZ;
-	public bool LockY;
-	public bool LockZ;
-	public bool useSmoothing;
-	public int player;
-	Transform target;
+	public Transform comTarget;
 
-	public GameObject hudElements;
-	#endregion
-
-	#region Private Properties
-	private Transform thisTransform;
-	private Vector3 velocity;
-	#endregion
-
-	bool hudActive = true;
-
-	private void Awake()
-	{
-		thisTransform = transform;
-		velocity = new Vector3(0.5f, 0.5f, 0.5f);
-		target = GameObject.Find ("2Handed Warrior").transform;
-
-		player = 1;
+	// Use this for initialization
+	void Start () {
+		fY = -45;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 
-	void Update()
+	void LateUpdate()
 	{
-		if(hudActive)
-		{
-			if (Input.GetKeyDown(KeyCode.H))
-			{
-				hudElements.SetActive (false);
-				hudActive = false;
-			}
+		Vector3 vPosition = comTarget.position;
+		fX += Input.GetAxis ("Mouse X") * fCameraSpeed;
+		fY += Input.GetAxis ("Mouse Y") * fCameraSpeed;
 
-		}
-		else
-		{
-			if (Input.GetKeyDown(KeyCode.H))
-			{
-				hudElements.SetActive (true);
-				hudActive = true;
-			}
-		}
-		if (Input.GetKeyDown (KeyCode.F1)) 
-		{
-			target = GameObject.Find ("2Handed Warrior").transform;
+		vPosition -= Quaternion.Euler (-fY, fX, 0.0f) * Vector3.forward * fDistance;
 
-			player = 1;
-			Debug.Log ("1");
-		} 
-		else if (Input.GetKeyDown (KeyCode.F2)) 
-		{
-			Debug.Log ("2");
-			target = GameObject.Find ("Mage Warrior").transform;
-
-			player = 2;
-		}
-		else if (Input.GetKeyDown (KeyCode.F3)) 
-		{
-			Debug.Log ("3");
-			target = GameObject.Find ("Archer Warrior").transform;
-
-			player = 3;
-		}
-	}
-
-	// ReSharper disable UnusedMember.Local
-	private void LateUpdate()
-	// ReSharper restore UnusedMember.Local
-	{
-		var newPos = Vector3.zero;
-
-
-			offSetZ = -6;
-		if (useSmoothing)
-		{
-			newPos.x = Mathf.SmoothDamp(thisTransform.position.x, target.position.x, ref velocity.x, SMOOTH_TIME);
-			newPos.y = Mathf.SmoothDamp(thisTransform.position.y, target.position.y+4, ref velocity.y, SMOOTH_TIME);
-			newPos.z = Mathf.SmoothDamp(thisTransform.position.z, target.position.z + offSetZ, ref velocity.z, SMOOTH_TIME);
-		}
-		else
-		{
-			newPos.x = target.position.x;
-			newPos.y = target.position.y;
-			newPos.z = target.position.z;
-		}
-		
-		#region Locks
-		if (LockX)
-		{
-			newPos.x = thisTransform.position.x;
-		}
-		
-		if (LockY)
-		{
-			newPos.y = thisTransform.position.y;
-		}
-		
-		if (LockZ)
-		{
-			newPos.z = thisTransform.position.z;
-		}
-		#endregion
-
-		transform.position = Vector3.Slerp(transform.position, newPos, Time.time);
-
-
+		transform.position = vPosition;
+		transform.LookAt (comTarget);
 	}
 }
---
+++ Assets/Script/cameramove_.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramove_ : MonoBehaviour {
+
+	#region Consts
+	private const float SMOOTH_TIME = 0.3f;
+	#endregion
+
+	#region Public Properties
+	public bool LockX;
+	float offSetZ;
+	public bool LockY;
+	public bool LockZ;
+	public bool useSmoothing;
+	public int player;
+	Transform target;
+
+	public GameObject hudElements;
+	#endregion
+
+	#region Private Properties
+	private Transform thisTransform;
+	private Vector3 velocity;
+	#endregion
+
+	bool hudActive = true;
+
+	private void Awake()
+	{
+		thisTransform = transform;
+		velocity = new Vector3(0.5f, 0.5f, 0.5f);
+		target = GameObject.Find ("2Handed Warrior").transform;
+
+		player = 1;
+	}
+
+	void Update()
+	{
+		if(hudActive)
+		{
+			if (Input.GetKeyDown(KeyCode.H))
+			{
+				hudElements.SetActive (false);
+				hudActive = false;
+			}
+
+		}
+		else
+		{
+			if (Input.GetKeyDown(KeyCode.H))
+			{
+				hudElements.SetActive (true);
+				hudActive = true;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.F1)) 
+		{
+			target = GameObject.Find ("2Handed Warrior").transform;
+
+			player = 1;
+			Debug.Log ("1");
+		} 
+		else if (Input.GetKeyDown (KeyCode.F2)) 
+		{
+			Debug.Log ("2");
+			target = GameObject.Find ("Mage Warrior").transform;
+
+			player = 2;
+		}
+		else if (Input.GetKeyDown (KeyCode.F3)) 
+		{
+			Debug.Log ("3");
+			target = GameObject.Find ("Archer Warrior").transform;
+
+			player = 3;
+		}
+	}
+
+	// ReSharper disable UnusedMember.Local
+	private void LateUpdate()
+	// ReSharper restore UnusedMember.Local
+	{
+		var newPos = Vector3.zero;
+
+
+			offSetZ = -6;
+		if (useSmoothing)
+		{
+			newPos.x = Mathf.SmoothDamp(thisTransform.position.x, target.position.x, ref velocity.x, SMOOTH_TIME);
+			newPos.y = Mathf.SmoothDamp(thisTransform.position.y, target.position.y+4, ref velocity.y, SMOOTH_TIME);
+			newPos.z = Mathf.SmoothDamp(thisTransform.position.z, target.position.z + offSetZ, ref velocity.z, SMOOTH_TIME);
+		}
+		else
+		{
+			newPos.x = target.position.x;
+			newPos.y = target.position.y;
+			newPos.z = target.position.z;
+		}
+		
+		#region Locks
+		if (LockX)
+		{
+			newPos.x = thisTransform.position.x;
+		}
+		
+		if (LockY)
+		{
+			newPos.y = thisTransform.position.y;
+		}
+		
+		if (LockZ)
+		{
+			newPos.z = thisTransform.position.z;
+		}
+		#endregion
+
+		transform.position = Vector3.Slerp(transform.position, newPos, Time.time);
+
+
+	}
+}
