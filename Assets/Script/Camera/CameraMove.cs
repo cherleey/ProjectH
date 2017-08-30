59e531e1431cc809392663934ddad9f71f035a16
cherleey <31400524+cherleey@users.noreply.github.com>
2017-08-30 13:36:33 +0900

    Mage 생성

--- Assets/Script/Camera/CameraMove.cs
+++ Assets/Script/Camera/CameraMove.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour {
+	
+	enum SelectedCharater {WARRIOR, MAGE, ARCHER};
 
+	Transform comTarget;
 	float fDistance = 10.0f;
 	float fX = 0.0f;
 	float fY = 0.0f;
 	float fCameraSpeed = 2.0f;
-
-	public Transform comTarget;
+	SelectedCharater eSelected;
 
 	// Use this for initialization
 	void Start () {
 		fY = -45;
+		eSelected = SelectedCharater.WARRIOR;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		CharacterChange ();
 	}
 
 	void LateUpdate()
@@ -36,5 +39,33 @@ public class CameraMove : MonoBehaviour {
 
 		transform.position = vPosition;
 		transform.LookAt (comTarget);
+	}
+
+	void CharacterChange()
+	{
+		if (Input.GetKeyDown (KeyCode.F1))
+			eSelected = SelectedCharater.WARRIOR;
+
+		if (Input.GetKeyDown (KeyCode.F2))
+			eSelected = SelectedCharater.MAGE;
+
+		if (Input.GetKeyDown (KeyCode.F3))
+			eSelected = SelectedCharater.ARCHER;
+
+		/*
+		switch (eSelected) {
+		case 0:
+			comTarget = GameObject.Find ("2Handed Warrior").transform;
+			break;
+
+		case 1:
+			comTarget = GameObject.Find ("Mage Warrior").transform;
+			break;
+
+		case 2:
+			comTarget = GameObject.Find ("Archer Warrior").transform;
+			break;
+		}
+		*/
 	}
 }
